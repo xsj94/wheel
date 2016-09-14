da273513232875f5446ba0ca8775e66b82bef2a1
xsj <xsj@qq.com>
2016-09-14 14:33:24 +0800

    抽奖

---
+++ js/js.js
@@ -0,0 +1,14 @@
+function fontAuto(width) {
+    var winW = document.documentElement.clientWidth;
+    if (winW >= width) {
+        document.documentElement.style.fontSize = "625%";
+    }
+    else {
+        document.documentElement.style.fontSize = (winW / width * 625) + "%";
+    }
+}
+fontAuto(640);
+window.onresize = function () {
+    fontAuto(640);
+};
+
